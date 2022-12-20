7d0f7864af3882b7bf2bbbe5032c72a49e8a6d32
MahmoudElattar1234 <94454076+MahmoudElattar1234@users.noreply.github.com>
2022-12-20 19:59:21 +0200

    test in scene delegate

--- VenueTask/App+Scene Delegates/SceneDelegate.swift
+++ VenueTask/App+Scene Delegates/SceneDelegate.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+ 
     var window: UIWindow?
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
+    let numbers: [[Int]?] = [[10, 20, 42, 3], [10, 20, 42, 3], [10, 20, 42, 3]]
+       let flaat = numbers.flatMap { item in
+            print("flatMap \(item)")
+            return item
+        }
+     let comm = numbers.compactMap { item in
+            print(" compactMap \(item)")
+            return item
+        }
+        print(flaat)
+        print(comm)
         
+//         let average: Int = numbers.reduce(0, &-)/numbers.count
+//         let average1: Int = numbers.reduce(0, &+)/numbers.count
+       
+//       let numbers: [Int] = [1000, 5670, 42, 21341123, 9_223_372_036_854_775_807]
+//        let average: Int = numbers.reduce(0, &-)/numbers.count
+//        let average1: Int = numbers.reduce(0, &+)/numbers.count
+//
+//        print("Result -> \(average1)")
+//        print("Result -> \(average)")
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         AppManager.shared.initWindow(window)
